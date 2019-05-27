ec94263b735e3cadb578a0b8a457bcc8d5617faa
tanisha03 <37444245+tanisha03@users.noreply.github.com>
2019-05-28 01:16:49 +0530

    Add files via upload

---
+++ DAA LAB/bfs.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int n,a[20][20],visited[20]={0},q[20]={0},f=0,r=0;
+void bfs(int s)
+{
+	for(int i=1;i<=n;i++)
+		if(a[s][i]!=0 && visited[i]==0)
+			q[r++]=i;
+	if(f<r)
+	{
+		if(!visited[q[f]])
+		{
+			visited[q[f]]=1;
+			printf("%d ",q[f]);
+		}
+		bfs(q[f++]);
+	}
+}
+int main()
+{
+	int i,j,source;
+	printf("enter the number of nodes\n");
+	scanf("%d",&n);
+	printf("enter the adjacency matrix\n");
+	for(i=1;i<=n;i++)
+		for(j=1;j<=n;j++)
+			scanf("%d",&a[i][j]);
+	printf("enter the source\n");
+	scanf("%d,",&source);
+	visited[source]=1;
+	printf("BFS traversal is :\n%d ",source);
+	bfs(source);
+	printf("\nnodes reachable from %d are:\n",source);
+	for(i=1;i<=n;i++)
+		if(i!=source)
+			if(visited[i])
+				printf("%d  ",i);
+
+	
+}
